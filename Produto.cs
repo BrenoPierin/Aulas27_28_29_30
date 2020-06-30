17c74e5ba8ffc651661a33cacec16bd1cea809b3
unknown <brenomax02@gmail.com>
2020-06-30 16:08:21 -0300

    lista finalizada

--- Produto.cs
+++ Produto.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+
 namespace Aula27_28_29_30
 {
     public class Produto
@@ -31,33 +33,76 @@ namespace Aula27_28_29_30
             File.AppendAllLines(PATH, linha);
         }
 
+        /// <summary>
+        /// Metodo paraler os produtos em lista
+        /// </summary>
+        /// <returns>Lista de produtos no arquivo csv e no console</returns>
         public List<Produto> Ler()
         {
             //lista criada
             List<Produto> produtos = new List<Produto>();
 
+            //Transformar as linhas em um array de string;
             string[] linhas = File.ReadAllLines(PATH);
 
+            //Varri o array em strings
             foreach( var linha in linhas )
             {
+                //Separei os termos entre os ';'
                 string[] dados = linha.Split(';');
 
+
+                //declarando um novo tipo de "produto" e tratando os resultados;
                 Produto p = new Produto();
                 p.Codigo  = int.Parse(SepararDado(dados[0]));
                 p.Nome  = SepararDado(dados[1]);
                 p.Preco  = float.Parse(SepararDado(dados[2]));
 
+                //Adicionei o produto na lista;
                 produtos.Add(p);
             }
+            foreach(var linha in linhas)
+            {
+                string[] dados = linha.Split(';');
+                
+                Produto produt = new Produto();
+                var y = produt.Nome.OrderBy(c => produt.Nome );
 
+                System.Console.WriteLine(y);
+                
+            }
+            //Retornei o produto;
             return produtos;
-
         }
 
+        // public void Listar()
+        // {
+        //     string[] prod = File.ReadAllLines(PATH);
+
+        //     foreach(var p in prod)
+        //     {
+        //         string[] dados = p.Split(';');
+                
+        //         Produto produt = new Produto();
+        //         var y = produt.Ler().OrderBy(c => c.Nome );
+
+        //         foreach(Produto pro in y)
+        //         {
+        //         System.Console.WriteLine(y);
+        //         }
+        //     }
+        // }
+
+        /// <summary>
+        /// metodo que separa os termos do "="
+        /// </summary>
+        /// <param name="dados">separa os dados do csv</param>
+        /// <returns>somente os valores da coluna</returns>
         public string SepararDado(string dados)
         {
             return dados.Split('=')[1];
         }
+
 
         private string PrepararLinhaCSV(Produto prod)
         {
